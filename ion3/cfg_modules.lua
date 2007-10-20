6e80cb18fd7fd9c64f15d5c667226bc4f105017c
Austin Clements <amdragon@mit.edu>
2007-10-20 05:39:50 +0000

    Xinerama support
    git-svn-id: http://awakening.csail.mit.edu/svn/amthrax/sys/dotfiles@1359 ee464a63-62dc-0310-8e92-97079e79a51f

--- ion3/cfg_modules.lua
+++ ion3/cfg_modules.lua
@@ -1,3 +1,5 @@
+dopath("mod_xinerama")
+
 dopath("mod_query")
 dopath("mod_menu")
 dopath("mod_tiling")
@@ -5,3 +7,4 @@ dopath("mod_tiling")
 dopath("mod_statusbar")
 --dopath("mod_dock")
 dopath("mod_sp")
+
