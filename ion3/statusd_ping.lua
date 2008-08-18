9af06c0b99e1836bb7fea787cdf7337feb740268
Austin Clements <amdragon@mit.edu>
2008-08-18 23:04:37 +0000

    Added support for polling the contents of .defaulthost and restarting ping with a new host when it changes
    git-svn-id: http://awakening.csail.mit.edu/svn/amthrax/sys/dotfiles@1774 ee464a63-62dc-0310-8e92-97079e79a51f

--- ion3/statusd_ping.lua
+++ ion3/statusd_ping.lua
@@ -2,8 +2,6 @@
 
 local timer = nil
 
---statusd.inform("ping_template", "000.000.000.000")
-
 local laststr = nil
 local lasthint = nil
 local function inform(str, hint)
@@ -21,20 +19,9 @@ local function warn(msg)
    statusd.warn(msg)
 end
 
-local lasthostcheck = nil
 local host = nil
-local function gethost()
-   local now = os.time()
-   if lasthostcheck == nil or os.difftime(now, lasthostcheck) > 10 then
-      local f, res = io.open("/mts/home1/aclements/.defaulthost", "r")
-      if not f then
-         warn(res)
-         return false
-      end
-      host = f:read("*l")
-   end
-   return true
-end
+local pingpid = nil
+local restartping = nil
 
 local function process(str)
    -- Skip header
@@ -76,52 +63,41 @@ local function process(str)
       str = coroutine.yield()
    end
    -- Ping quit
-   warn("Ping exited unexpectedly")
-   -- XXX Restart ping
+   pingpid = nil
+   if restartping then
+      startping(restartping)
+      restartping = nil
+   else
+      warn("Ping exited unexpectedly")
+      -- Restart ping on the next gethost
+      host = nil
+   end
 end
 
+function startping(newhost)
+   if pingpid then
+      restartping = newhost
+      pcall(os.execute, "kill "..pingpid)
+   else
+      host = newhost
+      pingpid = statusd.popen_bgread("exec ping -f -i 1 "..host,
+                                     coroutine.wrap(process),
+                                     coroutine.wrap(process))
+   end
+end
+
+local function gethost()
+   local f, res = io.open("/mts/home1/aclements/.defaulthost", "r")
+   if not f then
+      inform("No .defaulthost", "important")
+   else
+      local newhost = f:read("*l")
+      if newhost ~= host then
+         startping(newhost)
+      end
+   end
+   timer:set(10*1000, gethost)
+end
+
+timer = statusd.create_timer()
 gethost()
-statusd.popen_bgread("ping -f -i 1 "..host,
-                     coroutine.wrap(process),
-                     coroutine.wrap(process))
-
-
--- local ping = nil
--- local count = 0
--- while true do
---    if not gethost() then
---       break
---    end
---    if ping == nil then
---       local success
---       success, ping = pcall(io.popen, "ping -f -i 1 "..host)
---       if not success then
---          warn("Failed to start ping")
---          break
---       end
---       -- Header
---       ping:read("*l")
---    end
-
---    ch = ping:read(1)
---    if ch == nil then
---       warn("Ping exited unexpectedly")
---       -- Restart ping
---       ping = nil
---    elseif ch == " " then
---       count = 0
---    elseif ch == "." or ch == "E" then
---       count = count + 1
---    elseif ch == "\b" then
---       -- Should be followed by " " or "E"
---    else
---       warn("Unknown ping output: \"" .. ch .. "\"")
---    end
---    if count >= 5 then
---       inform(host, "critical")
---    elseif count >= 3 then
---       inform(host, "important")
---    else
---       inform(host, "normal")
---    end
--- end
